1825135cda70f4a758d1f9798cbb5f8f62318f4f
Deceit-87 <manuelfreund318@gmail.com>
2022-02-22 00:46:06 +0100

    fine prima  parte

--- js/app.js
+++ js/app.js
@@ -1,46 +1,83 @@
-const GRID = document.getElementById('container');
+const GRID_EL = document.getElementById('grid');
 const PLAY_BUTTON = document.getElementById('play-btn');
 const SELECT = document.getElementById('difficult-level');
-const DIFFICULT = SELECT.value
 
-let rows;
-let cols; 
+// let rows;
+// let cols; 
+// let cellSize;  
+let rows, cols, cellSize;
 
 PLAY_BUTTON.addEventListener('click',() =>{
-    console.log(SELECT.value)
+    
+const DIFFICULT = SELECT.value;
+switch ( DIFFICULT ) {
+
+    // case 'Easy':
+    //     // rows = 10 ;
+    //     // cols = 10 ;
+    //     rows = cols = 10;
+    // break;
+
+
+    case 'Medium':
+        // rows = 9 ;
+        // cols = 9 ;
+        rows = cols = 9;
+    break;
+
+    case 'Hard':
+        // rows = 7 ;
+        // cols = 7 ;
+        rows = cols = 7;
+    break;
+    default:
+        rows = 10 ;
+        cols = 10 ;
+}
+ 
+    
+const CELL_NUMBERS = rows * cols;
+cellSize = `calc(100% / ${cols})`;
+GRID_EL.innerHTML = '';
+const callBack =function (){
+    console.log('mi hai clickato!');
+    this.classList.add('selected')
+}
+
+
+            //   GENERAZIONE GRIGLIA
+
+
+    for (let i = 0; i < CELL_NUMBERS; i++) {
+        
+        const CELL = document.createElement('div');
+        CELL.style.width = cellSize;
+        CELL.append(i + 1);
+        CELL.classList.add('cell');
+        
+        GRID_EL.appendChild(CELL);
+       
+    
+        console.log(CELL)
+        CELL.addEventListener('click',callBack)
+    }
+    
 
 })
 
 
 
 
-                           // GRIGLIA 
-switch ( DIFFICULT ) {
-
-    case 'Easy':
-        rows = 10 ;
-        cols = 10 ;
-    break;
+    
 
 
-    case 'Medium':
-        rows = 9 ;
-        cols = 9 ;
-    break;
+                           
 
-    case 'Hard':
-        rows = 7 ;
-        cols = 7 ;
-    break;
-    default:
-        rows = 10 ;
-        cols = 10 ;
-}
 
 
 
  
-const CELL_NUMBERS = rows * cols;
 
 
-console.log(rows,cols,CELL_NUMBERS);
+
+
