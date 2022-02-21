459b8ce3196b8c02e62105d08b173cdd708bc618
Deceit-87 <manuelfreund318@gmail.com>
2022-02-21 19:44:23 +0100

    aggiunto css

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const grid = document.getElementById('container')
+
+
+const gridCell = []
+
+
+for (let i = 0; i < 100; i++) { 
+    
+    let cell = [i]
+  
+    gridCell.push(cell)
+}
+console.log(gridCell)
